d77a09c88b2a39f6e514e5131169879706a77096
Dworv <dwarvyt@gmail.com>
2022-11-29 18:58:39 -0800

    feat: css

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn home() -> rocket::response::content::RawHtml<String> {
+    rocket::response::content::RawHtml(String::from(r#"<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css"
+</head>
+<body>
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+</body>
+</html>"#))
+}
+
+#[get("/style.css")]
+fn style() -> rocket::response::content::RawCss<String> {
+    rocket::response::content::RawCss(std::fs::read_to_string("style.css").unwrap())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![home, style])
 }
