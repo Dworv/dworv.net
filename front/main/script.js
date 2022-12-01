a69a3c88986fd79d821fd1b911ee80e075c51a13
Dworv <dwarvyt@gmail.com>
2022-11-30 22:44:25 -0800

    fix: js now loads correctly

--- front/main/script.js
+++ front/main/script.js
@@ -1,32 +1,36 @@
-const movie = document.getElementById("movie");
+window.onload = () => {
+  console.log("epic website moment");
 
-const orCycle = function* () {
-  let c = 1;
-  let d = +1;
+  const movie = document.getElementById("movie");
   
-  const START = 1;
-  const END = 3;
+  const orCycle = function* () {
+    let c = 1;
+    let d = +1;
+    
+    const START = 1;
+    const END = 3;
+    
+    while (true) {
+      if (c == START) {
+        d = 1;
+      }
+      if (c == END) {
+        d = -1;
+      }
+      c += d;
+      yield c;
+    }
+  };
   
-  while (true) {
-    if (c == START) {
-      d = 1;
-    }
-    if (c == END) {
-      d = -1;
-    }
-    c += d;
-    yield c;
-  }
-};
-
-let cycle = orCycle();
-setInterval(() => {
-    let o = cycle.next();
-    movie.dataset.orientation = o.value;
-}, 2000)
-
-let follow = document.getElementById("github-follow");
-let src = document.getElementById("source-code");
-
-follow.onclick = () => {window.location.href("https://github.com/dworv")}
-src.onclick = () => {window.location.href("https://github.com/dworv/dworv.net")}
+  var follow = document.getElementById("github-follow");
+  var src = document.getElementById("source-code");
+  
+  follow.onclick = () => {window.location.href = "https://github.com/dworv"}
+  src.onclick = () => {window.location.href = "https://github.com/dworv/dworv.net"}
+  
+  let cycle = orCycle();
+  setInterval(() => {
+      let o = cycle.next();
+      movie.dataset.orientation = o.value;
+  }, 2000)
+}
